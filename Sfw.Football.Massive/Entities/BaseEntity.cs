170401481f1cd69ab729547561f5f657526a111f
Matt George <jack.bauer.loves.matt@googlemail.com>
2015-08-17 19:40:36 +0100

    Added "Players" entity

---
+++ Sfw.Football.Massive/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using Massive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sfw.Football.Massive.Entities
+{
+    public class BaseEntity : DynamicModel
+    {
+        public BaseEntity() : base("DefaultConnection")
+        {
+            PrimaryKeyField = "Id";
+        }
+    }
+}
