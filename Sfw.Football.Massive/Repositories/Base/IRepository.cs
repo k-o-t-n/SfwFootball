1e88d26bfbfaae272bd38e5d6e44d4dd67c079cb
Matt George <jack.bauer.loves.matt@googlemail.com>
2015-08-17 21:51:45 +0100

    Added missing IRepository class

---
+++ Sfw.Football.Massive/Repositories/Base/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Sfw.Football.Massive.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sfw.Football.Massive.Repositories.Base
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IEnumerable<dynamic> All();
+    }
+}
