c017df3e2294e4cd2d674f20c82aabdb80e1a902
Matt Smith <matt.d.smith.89@gmail.com>
2015-08-17 18:14:47 +0100

    Initial commit

---
+++ Sfw.Football/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sfw.Football.Startup))]
+namespace Sfw.Football
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
