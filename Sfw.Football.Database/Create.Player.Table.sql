439f388c9182e52615b6ac547caccfb4be38ebe3
Matt Smith <matt.d.smith.89@gmail.com>
2015-08-19 21:14:04 +0100

    Added updated ratings

--- Sfw.Football.Database/Create.Player.Table.sql
+++ Sfw.Football.Database/Create.Player.Table.sql
@@ -7,19 +7,25 @@ CREATE TABLE IF NOT EXISTS `player` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
+-- ratings correct as of 19-08-2015
+
 INSERT INTO `sfwfootballdev`.`player`
 (`Id`,
 `Name`,
 `Rating`)
 VALUES
-(1,"Matt Smith",0),
-(2,"Matt George",0),
-(3,"David Raine",0),
-(4,"James Beckenham",0),
+(1,"Matt Smith",-1),
+(2,"Matt George",-1),
+(3,"David Raine",1),
+(4,"James Beckenham",-1),
 (5,"Matt Overton",0),
-(6,"Mike Murray",0),
-(7,"Carlo Veo",0),
+(6,"Mike Murray",-1),
+(7,"Carlo Veo",1),
 (8,"Rick Powell",0),
-(9,"Richard Press",0),
-(10,"Andrew Von Gerard",0),
-(11,"Joseph Freeston?",0);
+(9,"Richard Press",1),
+(10,"Andrew von Gerard",0),
+(11,"Joseph Freeston",-1),
+(12,"Nicky Simillie",1),
+(13,"Dave Horsley",0),
+(14,"Ben Turner",1),
+(15,"Alan Gentle",0);
