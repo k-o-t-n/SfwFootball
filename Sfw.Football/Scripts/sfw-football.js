85b387310e0e3114aae49fdad301a0bb9915c1f8
Matt George <jack.bauer.loves.matt@googlemail.com>
2015-08-22 12:13:56 +0100

    Changed row selection to use CSS

--- Sfw.Football/Scripts/sfw-football.js
+++ Sfw.Football/Scripts/sfw-football.js
@@ -1,17 +1,11 @@
-﻿//$(document).ready(function ($) {
-//    $(".clickable-row").click(function () {
-//        var checkBox = $('#' + $(this).data("checkBoxId"));
-//        checkBox.prop("checked", !checkBox.prop("checked"));
-//    });
-//});
-
-$(document).ready(function ($) {
-    var test = pageGrids.allPlayersGrid;
-});
-
-$(function () {
+﻿$(function () {
     pageGrids.allPlayersGrid.onRowSelect(function (e) {
+        // Flip the checkbox
         var checkBox = $('#CheckBox' + e.row.Id);
         checkBox.prop("checked", !checkBox.prop("checked"));
+        // Get closest row to checkbox
+        var selectedRow = checkBox.closest('tr');
+        // Toggle class on row
+        selectedRow.toggleClass("grid-row-selected");
     });
 });
