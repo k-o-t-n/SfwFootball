2bad5497e40989d4de8dc401635cd665d40f608c
Matt Smith <matt.d.smith.89@gmail.com>
2015-08-23 12:17:05 +0100

    More tidying, removed default template identity packages.

--- Sfw.Football/Startup.cs
+++ Sfw.Football/Startup.cs
@@ -1,20 +1,6 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
-using Owin;
-
-[assembly: OwinStartupAttribute(typeof(Sfw.Football.Startup))]
-namespace Sfw.Football
+﻿namespace Sfw.Football
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
-        {
-            app.UseCookieAuthentication(new CookieAuthenticationOptions()
-            {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/admin/login")
-            });
-        }
     }
 }
