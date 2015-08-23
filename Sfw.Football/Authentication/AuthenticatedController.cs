2e0f553e419ad85352eabaa54ac096cd4c313467
Matt Smith <matt.d.smith.89@gmail.com>
2015-08-23 13:53:28 +0100

    fixup

---
+++ Sfw.Football/Authentication/AuthenticatedController.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace Sfw.Football.Authentication
+{
+    public abstract class AuthenticatedController : Controller
+    {
+        public AuthenticatedUser CurrentUser
+        {
+            get
+            {
+                return new AuthenticatedUser(this.User as ClaimsPrincipal);
+            }
+        }
+    }
+}
