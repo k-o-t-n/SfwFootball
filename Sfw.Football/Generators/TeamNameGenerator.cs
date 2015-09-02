44c874c24c4d759102c0dd4cc609e80529418d92
Matt George <jack.bauer.loves.matt@googlemail.com>
2015-09-02 23:02:50 +0100

    Added team name generation, tidied up team display model

---
+++ Sfw.Football/Generators/TeamNameGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Sfw.Football.Generators
+{
+    public class TeamNameGenerator : ITeamNameGenerator
+    {
+        public Tuple<string, string> GenerateTeamNames()
+        {
+            List<string> animals = Constants.TeamNames.Animals;
+            var random = new Random();
+            int index1 = random.Next(animals.Count);
+            int index2 = index1;
+            while (index2 == index1)
+            {
+                index2 = random.Next(animals.Count);
+            }
+            string name1 = animals[index1];
+            string name2 = animals[index2];
+            return new Tuple<string, string>(name1, name2);
+        }
+    }
+}
